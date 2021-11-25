bc537a875b464003f3797271a814226e7b615b8a
junhyeong <>
2021-11-25 18:00:28 +0900

    commit: 업데이트

---
+++ components/Svg.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Svg = () => {
+  return <></>;
+};
+
+export default Svg;
---
+++ data/data.js
@@ -0,0 +1,108 @@
+export const ListData = [
+	{
+		groupUid: 1,
+		name: 'Group1',
+		items: [
+			{
+				itemUid: 1,
+				name: 'Item1',
+			},
+			{
+				itemUid: 2,
+				name: 'Item2',
+			},
+		],
+	},
+	{
+		groupUid: 2,
+		name: 'Group2',
+		items: [
+			{
+				itemUid: 3,
+				name: 'Item3',
+			},
+			{
+				itemUid: 4,
+				name: 'Item4',
+			},
+		],
+	},
+	{
+		groupUid: 3,
+		name: 'Group3',
+		items: [],
+	},
+	{
+		groupUid: 4,
+		name: 'Group4',
+		items: [
+			{
+				itemUid: 5,
+				name: 'Item5',
+			},
+			{
+				itemUid: 6,
+				name: 'Item6',
+			},
+			{
+				itemUid: 7,
+				name: 'Item7',
+			},
+			{
+				itemUid: 8,
+				name: 'Item8',
+			},
+		],
+	},
+	{
+		groupUid: 5,
+		name: 'Group5',
+		items: [
+			{
+				itemUid: 9,
+				name: 'Item9',
+			},
+		],
+	},
+	{
+		groupUid: 6,
+		name: 'Group6',
+		items: [],
+	},
+	{
+		groupUid: 7,
+		name: 'Group7',
+		items: [
+			{
+				itemUid: 10,
+				name: 'Item10',
+			},
+			{
+				itemUid: 11,
+				name: 'Item11',
+			},
+		],
+	},
+	// 그룹에 속하지 않는 아이템
+	{
+		groupUid: null,
+		items: [
+			{
+				itemUid: 12,
+				name: 'Item12',
+			},
+			{
+				itemUid: 13,
+				name: 'Item13',
+			},
+			{
+				itemUid: 14,
+				name: 'Item14',
+			},
+			{
+				itemUid: 15,
+				name: 'Item15',
+			},
+		],
+	},
+];
