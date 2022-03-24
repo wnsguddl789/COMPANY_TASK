03312d02a9e4ef857fc7c4013a58f6c685d8dcdf
wnsguddl789 <wnsguddl789@gmail.com>
2022-03-24 10:03:36 +0900

    Layout init

---
+++ src/components/Layout/Layout.style.ts
@@ -0,0 +1,20 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div``;
+export const Header = styled.header`
+  width: 100vw;
+  height: 80px;
+  background-color: rgba(0, 0, 0, 0.2);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+export const HeaderList = styled.ul`
+  display: flex;
+`;
+export const HeaderItem = styled.li``;
+export const Main = styled.main`
+  width: 100%;
+  min-height: calc(100vh - 120px);
+  padding: 20px 0;
+`;
--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,6 +1,15 @@
 import Head from 'next/head';
 import * as React from 'react';
 
+import {
+  Main,
+  Container,
+  Header,
+  HeaderList,
+  HeaderItem,
+} from './Layout.style';
+import Link from 'next/link';
+
 export default class Layout extends React.Component {
   constructor(props) {
     super(props);
@@ -13,7 +22,24 @@ export default class Layout extends React.Component {
         <Head>
           <title>HI</title>
         </Head>
-        <main>{children}</main>
+        <Container>
+          <Header>
+            <HeaderList>
+              <p>파일럿 프로젝트</p>
+              <HeaderItem>
+                <Link href={'/'} passHref>
+                  홈
+                </Link>
+              </HeaderItem>
+              <HeaderItem>
+                <Link href={'/signIn'} passHref>
+                  로그인
+                </Link>
+              </HeaderItem>
+            </HeaderList>
+          </Header>
+          <Main>{children}</Main>
+        </Container>
       </React.Fragment>
     );
   }
